cc6418e509bf59af0fe19236e36fd19da46a404d
Adrienn <adrienn.d91@gmail.com>
2018-02-06 13:45:29 +0100

    Implemented show boards on main page

--- static/js/data_handler.js
+++ static/js/data_handler.js
@@ -9,18 +9,20 @@ dataHandler = {
     _loadData: function() {
         // it is not called from outside
         // loads data from local storage, parses it and put into this._data property
-        this._data = JSON.parse(localStorage.getItem(keyInLocalStorage))
+        this._data = JSON.parse(localStorage.getItem(this.keyInLocalStorage));
     },
     _saveData: function() {
         // it is not called from outside
         // saves the data from this._data to local storage
-        localStorage.setItem(keyInLocalStorage, JSON.stringify(this._data))
+        localStorage.setItem(this.keyInLocalStorage, JSON.stringify(this._data));
     },
     init: function() {
         this._loadData();
     },
     getBoards: function(callback) {
         // the boards are retrieved and then the callback function is called with the boards
+        let boards = this._data.boards;
+        callback(boards);
     },
     getBoard: function(boardId, callback) {
         // the board is retrieved and then the callback function is called with the board
--- static/js/dom.js
+++ static/js/dom.js
@@ -2,11 +2,24 @@
 dom = {
     loadBoards: function() {
         dataHandler.init();
+        dataHandler.getBoards(this.showBoards);
         // retrieves boards and makes showBoards called
     },
     showBoards: function(boards) {
         // shows boards appending them to #boards div
         // it adds necessary event listeners also
+        let numberOfBoards = boards.length;
+        let boardDiv = document.getElementById("boards");
+        for(let i=0; i<numberOfBoards; i++){
+            let newDivForBoard = document.createElement("div");
+            newDivForBoard.innerHTML = boards[i].title;
+            newDivForBoard.classList.add("row", "card", "bg-info");
+            boardDiv.appendChild(newDivForBoard);
+            let newButton = document.createElement("i");
+            newButton.classList.add("fas", "fa-angle-down");
+            newDivForBoard.appendChild(newButton);
+        }
+
     },
     loadCards: function(boardId) {
         // retrieves cards and makes showCards called
